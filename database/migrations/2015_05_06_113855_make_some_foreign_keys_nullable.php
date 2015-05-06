06b1e55fc789cd825092d9910c6ef9950c955349
John Wu <webmaster@leapoahead.com>
2015-05-06 19:41:52 +0800

    zaccount_id should be nullable

---
+++ database/migrations/2015_05_06_113855_make_some_foreign_keys_nullable.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeSomeForeignKeysNullable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::statement('ALTER TABLE `users` MODIFY `zaccount_id` INTEGER UNSIGNED NULL');
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        DB::statement('ALTER TABLE `users` MODIFY `zaccount_id` INTEGER UNSIGNED');
+	}
+
+}
