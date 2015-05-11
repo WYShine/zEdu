832472f2db01bf1438350b1e435885b55d7752be
John Wu <webmaster@leapoahead.com>
2015-05-11 13:01:10 +0800

    state filter in application management

--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -1,26 +1,45 @@
 @extends('app-menu')
 
 @section('content')
-<table class="table">
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Pattern</th>
-        <th>Status</th>
-        <th>Operations</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach ($zcourses as $zcourse)
+
+<header>
+    <div class="dropdown">
+        <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            State: {{$state}}
+            <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+            <li><a href="{{URL::current().'?state='.\App\Zcourse::STATE_PENDING}}">{{\App\Zcourse::STATE_PENDING}}</a></li>
+            <li><a href="{{URL::current().'?state='.\App\Zcourse::STATE_USING}}">{{\App\Zcourse::STATE_USING}}</a></li>
+            <li><a href="{{URL::current().'?state='.\App\Zcourse::STATE_CLOSED}}">{{\App\Zcourse::STATE_CLOSED}}</a></li>
+            <li><a href="{{URL::current().'?state='.\App\Zcourse::STATE_DISAPPROVED}}">{{\App\Zcourse::STATE_DISAPPROVED}}</a></li>
+        </ul>
+    </div>
+</header>
+
+<div>
+    <table class="table">
+        <thead>
         <tr>
-            <td>{{$zcourse->id}}</td>
-            <td>{{$zcourse->name}}</td>
-            <td>{{$zcourse->zpattern->description}}</td>
-            <td>{{$zcourse->state}}</td>
-            <td>@include ('admin.courses._index_operations')</td>
+            <th>#</th>
+            <th>Name</th>
+            <th>Pattern</th>
+            <th>Status</th>
+            <th>Operations</th>
         </tr>
-    @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        @foreach ($zcourses as $zcourse)
+            <tr>
+                <td>{{$zcourse->id}}</td>
+                <td>{{$zcourse->name}}</td>
+                <td>{{$zcourse->zpattern->description}}</td>
+                <td>{{$zcourse->state}}</td>
+                <td>@include ('admin.courses._index_operations')</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
+
 @endsection
