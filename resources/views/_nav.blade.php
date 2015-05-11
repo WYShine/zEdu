2c7bfb54a0861aeb6ff5b83d7962c44310b8ca49
John Wu <webmaster@leapoahead.com>
2015-05-11 09:12:27 +0800

    nav structure

--- resources/views/_nav.blade.php
+++ resources/views/_nav.blade.php
@@ -1,5 +1,10 @@
 <ul class="nav nav-pills nav-stacked">
     <li role="presentation" class={{$nav_title=="Home"?"active":""}}>
-        <a href="#">Home</a>
+        <a href="{{URL::route('home')}}">Home</a>
     </li>
+    @if (Auth::user()->role === "teacher")
+        <li role="presentation" class={{$nav_title=="New Application"?"active":""}}>
+            <a href="{{URL::route('teacher.courses.create')}}">New Application</a>
+        </li>
+    @endif
 </ul>
