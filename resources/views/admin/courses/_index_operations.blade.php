0a908ad2c74dc184c42348ae9f0c4aabc857b01e
John Wu <webmaster@leapoahead.com>
2015-05-16 00:10:11 +0800

    can close class - admin

--- resources/views/admin/courses/_index_operations.blade.php
+++ resources/views/admin/courses/_index_operations.blade.php
@@ -6,7 +6,7 @@
         ]) !!}
         {!!Form::hidden('state','using')!!}
         <div class="form-group">
-            {!! Form::submit('Approve', ['class'=>'btn btn-primary btn-sm form-control']) !!}
+            {!! Form::submit('Approve', ['class'=>'btn btn-sm btn-link']) !!}
         </div>
     {!! Form::close() !!}
 
@@ -17,7 +17,20 @@
     ]) !!}
     {!!Form::hidden('state','disapproved')!!}
     <div class="form-group">
-        {!! Form::submit('Disapprove', ['class'=>'btn btn-primary btn-sm form-control']) !!}
+        {!! Form::submit('Disapprove', ['class'=>'btn btn-sm btn-link']) !!}
+    </div>
+    {!! Form::close() !!}
+@endif
+
+@if ($state === 'using')
+    {!! Form::open([
+    'class' => 'form-inline form-inline-self',
+    'method' => 'PUT',
+    'url' => '/admin/courses/' . $zcourse->id
+    ]) !!}
+    {!!Form::hidden('state','closed')!!}
+    <div class="form-group">
+        {!! Form::submit('Close', ['class'=>'btn btn-sm btn-link']) !!}
     </div>
     {!! Form::close() !!}
 @endif
