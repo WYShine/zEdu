000be19a271462813d520fae6dd5e46b49938b95
John Wu <webmaster@leapoahead.com>
2015-05-20 20:51:26 +0800

    wudi

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,5 +1,31 @@
 @extends('app-menu')
 
 @section('content')
-    <h3>User Profile - {{$user->name}}</h3>
+    <h3>Profile - {{$user->name}}</h3>
+    <hr/>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Basic Information</h3>
+        </div>
+        <div class="panel-body">
+            <ul>
+                <li>Email: {{$user->email}}</li>
+                <li>Phone: {{$user->phone}}</li>
+                <li>Gender: {{$user->gender}}</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Work Information</h3>
+        </div>
+        <div class="panel-body">
+            <ul>
+                <li>Organization: {{$user->organization}}</li>
+                <li>Department: {{$user->department}}</li>
+            </ul>
+        </div>
+    </div>
 @endsection
